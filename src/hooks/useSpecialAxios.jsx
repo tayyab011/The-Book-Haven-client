c2d8451570d8b1e93ca7daf063e2412ee31b2bf3
tayyab011 <129363802+tayyab011@users.noreply.github.com>
2025-11-08 22:02:59 +0600

    add basic file folder structure ,npm package etc

---
+++ src/hooks/useSpecialAxios.jsx
@@ -0,0 +1,55 @@
+import axios from "axios";
+import { use, useEffect } from "react";
+import { AuthContext } from "../provider/AuthContext";
+import { useNavigate } from "react-router";
+
+const specialInstance = axios.create({
+  baseURL: "http://localhost:5050",
+});
+
+const useSpecialAxios = () => {
+const navigate=useNavigate()
+    const { user, logout } = use(AuthContext);
+  useEffect(() => {
+    //for request
+    if (user?.accessToken) {
+      const requestInterceptor = specialInstance.interceptors.request.use(
+        (config) => {
+          config.headers.authorization = `Bearer ${user.accessToken}`;
+          return config;
+        }
+      );
+      //for response
+      const responseInterceptor = specialInstance.interceptors.response.use(
+        (res) => {
+          return res;
+        },
+        (err) => {
+          const status = err.status;
+          if (status === 401 || status === 403) {
+            logout().then(() => {
+              navigate("/login");
+            });
+          }
+          console.log("special Axios", err);
+        }
+      );
+
+
+
+//for unmount
+return () => {
+  specialInstance.interceptors.request.eject(requestInterceptor);
+  specialInstance.interceptors.response.eject(responseInterceptor);
+};
+
+
+    }
+
+   
+    
+  }, [user, logout, navigate]);
+     return specialInstance; 
+};
+
+export default useSpecialAxios;
---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+import RootLayout from './Layout/RootLayout.jsx';
+import Home from './components/Home.jsx';
+
+import AuthProvider from './provider/AuthProvider.jsx';
+import Login from './components/Login';
+import Register from './components/Register.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+     
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
+);
